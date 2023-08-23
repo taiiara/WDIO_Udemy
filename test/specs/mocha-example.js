5a8ae0107fdea9b6053b11864ba5d848a7f7f4f0
taiiara <taiara.regis@objectedge.com>
2023-08-22 22:18:31 -0300

    Update

--- test/specs/mocha-example.js
+++ test/specs/mocha-example.js
@@ -2,8 +2,14 @@ describe("Description of test suite", () => {
   before(() => {
     console.log("Runs once before the first test in this block");
   });
+  beforeEach(() => {
+    console.log("Runs once before each test in this block");
+  });
   after(() => {
     console.log("Runs once after the last test in this block");
+  });
+  afterEach(() => {
+    console.log("Runs once after each test in this block");
   });
   it("Individual test 1", () => {
     console.log("Execute code: Individual test 1");
